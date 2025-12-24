5da853c3b0cc1d5d9413d3ec24758af01e161af6
GrayHoodie <matrala0419@gmail.com>
2025-12-24 02:32:34 -0600

    converting to animation player

--- scripts/baurino.gd
+++ scripts/baurino.gd
@@ -1,46 +1,50 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 
 func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_axis("move_left", "move_right")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		animated_sprite.play("jump")
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		
-		velocity.x = direction * SPEED
-		
-		animated_sprite.flip_h = direction < 0
-		
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-	if animated_sprite.is_on_floor():
-		if direction == 0:
+		if not direction:
 			if Input.is_action_pressed("flatten"):
 				animated_sprite.play("loaf")
+				_on_animated_sprite_2d_animation_finished()
 			else:
 				animated_sprite.play("idle_stnd")
 		elif Input.is_action_pressed("run"):
 			animated_sprite.play("run")
 		else:
 			animated_sprite.play("walk")
-	else:
+
+	# Handle jump.d
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 		animated_sprite.play("jump")
+		_on_animated_sprite_2d_animation_finished()
+		
+	# if moving
+	if direction:
+		# move in the direction you're going, at your character's speed
+		velocity.x = direction * SPEED
+		# flip horizontal (left) if direction == -1
+		animated_sprite.flip_h = direction < 0
+	# not moving
+	else:
+		# slow down from moving?? probably
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	pass
