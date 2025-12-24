9009a7cbefffc51838589d24776e75cf1e7c2f61
GrayHooodie <matrala0419@gmail.com>
2025-12-24 01:01:48 -0600

    trying to fix animations

--- scripts/baurino.gd
+++ scripts/baurino.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
 
 @onready var animated_sprite = $AnimatedSprite2D
 
@@ -12,14 +13,23 @@ func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.d
+	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		animated_sprite.play("jump")
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("move_left", "move_right")
-	
+	var direction := Input.get_axis("ui_left", "ui_right")
+	if direction:
+		
+		velocity.x = direction * SPEED
+		
+		animated_sprite.flip_h = direction < 0
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
 	if animated_sprite.is_on_floor():
 		if direction == 0:
 			if Input.is_action_pressed("flatten"):
@@ -32,17 +42,5 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("walk")
 	else:
 		animated_sprite.play("jump")
-		animated_sprite.queue("air")
-		
-	# if moving
-	if direction:
-		# move in the direction you're going, at your character's speed
-		velocity.x = direction * SPEED
-		# flip horizontal (left) if direction == -1
-		animated_sprite.flip_h = direction < 0
-	# not moving
-	else:
-		# slow down from moving?? probably
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
